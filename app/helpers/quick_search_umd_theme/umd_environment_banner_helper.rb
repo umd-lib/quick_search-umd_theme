bb8a801e4b3f4f6e4793c7e436b194520f036a28
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2018-04-20 20:16:21 +0200

    LIBSEARCH-24 Slight changes to env banner and remove rubocop binstub
    Revmoe the center align for the evn banner text, remove the #humanize
    when using the ENVIRONMENT_BANNER env. Also remove the rubocop binstub.

--- app/helpers/quick_search_umd_theme/umd_environment_banner_helper.rb
+++ app/helpers/quick_search_umd_theme/umd_environment_banner_helper.rb
@@ -7,7 +7,7 @@ module QuickSearchUmdTheme
   # https://confluence.umd.edu/display/LIB/Create+Environment+Banners
   module UmdEnvironmentBannerHelper
     ENVIRONMENT_NAME = if ENV['ENVIRONMENT_BANNER']
-                         ENV['ENVIRONMENT_BANNER'].humanize.freeze
+                         ENV['ENVIRONMENT_BANNER'].freeze
                        elsif Rails.env.development?
                          'Local'
                        elsif Socket.gethostname =~ /(local|dev|stage)\./
@@ -20,9 +20,9 @@ module QuickSearchUmdTheme
 
     def environment_banner
       return unless ENVIRONMENT_NAME
-      content_tag(:div, "#{ENVIRONMENT_NAME.humanize} Environment",
+      content_tag(:div, "#{ENVIRONMENT_NAME} Environment",
                   class: 'environment-banner',
-                  id: "environment-#{ENVIRONMENT_NAME.downcase}")
+                  id: "environment-#{ENVIRONMENT_NAME.gsub(/\W/i, '_').underscore}")
     end
   end
 end
